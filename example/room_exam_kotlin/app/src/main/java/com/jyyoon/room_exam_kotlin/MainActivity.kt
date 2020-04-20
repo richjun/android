50be2bcafab4160f45cb230492f75243f3ee49d3
Junsuk Oh <a811219@gmail.com>
2020-04-20 16:51:06 +0900

    에러 수정

--- example/room_exam_kotlin/app/src/main/java/com/jyyoon/room_exam_kotlin/MainActivity.kt
+++ example/room_exam_kotlin/app/src/main/java/com/jyyoon/room_exam_kotlin/MainActivity.kt
@@ -1,19 +1,18 @@
 package com.jyyoon.room_exam_kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
 import com.jyyoon.room_exam_kotlin.databinding.ActivityMainBinding
 
 class  MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         val binding =  DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val viewModel: MainViewModel by viewModels()
         binding.lifecycleOwner = this //이거 안하면 livedata 관찰할때 xml이 refresh가 안됨
         binding.viewModel = viewModel
     }
